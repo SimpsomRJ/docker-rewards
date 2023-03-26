2a35ca38fc34f3f92451e13b42115e0527004942
Eduardo Almeida <misc@edtatech.com>
2023-03-26 18:45:17 -0400

    Adjusted run.sh for multiple accounts

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 #rm -R bing-rewards
 #git clone https://github.com/jjjchens235/bing-rewards.git
 #pip install -r bing-rewards/BingRewards/requirements.txt
-cd /rewards/bing-rewards/
+cd /bing-rewards/
 /bin/bash update.sh
 cd /rewards/bing-rewards/BingRewards
 
 if [ $# -eq 0 ] ; then
-    echo "Syntax: ./run.sh '<original BingRewards.py parameters>' <email> <password> <telegram_token> <telegram_user_id>"
+    echo "Syntax: ./run.sh '<original BingRewards.py parameters>' '<email1> <email2> ... <emailN>' '<password1> <password2> ... <passwordN>' <telegram_token> <telegram_user_id>"
     exit 1
 fi
 
@@ -16,30 +16,29 @@ params=$( echo $1 )
 
 if [ $# -gt 1 ]
 then
-    email=$( echo $2 | base64 )
+    email=$( echo $2 )
 fi
 
 if [ $# -gt 2 ]
 then
-    pass=$( echo $3 | base64 )
+    pass=( $3 )
+    passwords=`printf "'%s' " "${pass[@]}"`
 fi
 
 if [ $# -gt 3 ]
 then
-    tat=$( echo $4 | base64 )
+    tat=$( echo $4 )
 fi
 
 if [ $# -gt 4 ]
 then
-    tu=$( echo $5 | base64 )
+    tu=$( echo $5 )
 fi
 
-if [ ! -f src/config.py ] ; then printf "credentials = dict(
-    email = '$email',
-    password = '$pass',
-    telegram_api_token = '$tat',
-    telegram_userid = '$tu'
-)" > src/config.py ; fi
+if [ ! -f config/config_multiple_accounts.json ] ; then 
+    python setup.py -e $email -p $passwords
+    python setup.py --telegram_api_token $tat--telegram_userid $tu
+fi
 
 sed -i 's/options.add_argument("--disable-gpu")/options.add_argument("--disable-gpu")\n        options.add_argument("--no-sandbox")\n        options.add_argument("--ipc=host")\n        options.add_argument("--disable-dev-shm-usage")\n/' src/driver.py
 sed -i "s/('telegram_api_token'))/('telegram_api_token')).replace('\\\n', '')/" BingRewards.py
